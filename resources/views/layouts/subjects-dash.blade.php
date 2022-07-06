8a26f0357ec093fbd968b0668db6d85b766b2695
ubanitc <tochukwu.ubani@gmail.com>
2022-07-06 07:51:29 +0100

    adjusted the title on dashboard page

--- resources/views/layouts/subjects-dash.blade.php
+++ resources/views/layouts/subjects-dash.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>Dashboard - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
+    <title>Dashboard- Imedidata</title>
     <!-- CSS files -->
     <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet"/>
     <link href="{{ asset('/dist/css/tabler-flags.min.css') }}" rel="stylesheet"/>
